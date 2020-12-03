2d6e7c9b0d1d627ab3108ed6e314d4fc11fbdaa2
fedgut <fedgut@gmail.com>
2020-12-03 17:35:33 -0600

    Solve about tringle 1 and 2

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  a, b, c = [a, b, c].sort
+  raise TriangleError if a <= 0 || a + b <= c
+  return :equilateral if a == b && b == c
+  return :isosceles if a == b || a == c || b == c
+
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
